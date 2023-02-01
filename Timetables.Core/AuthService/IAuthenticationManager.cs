ce2335abe7bd569e27f1ee1f8e8ddded8d4c5c4a
AlexGubkovich <alex.gubkovich@gmail.com>
2023-02-01 15:23:37 +0300

    add authenticationManager

---
+++ Timetables.Core/AuthService/IAuthenticationManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Timetables.Core.DTOs.UserDTOs;
+
+namespace Timetables.Core.AuthService
+{
+    public interface IAuthenticationManager
+    {
+        Task<bool> ValidateUser(UserForAuthenticationDTO userForAuth);
+        Task<string> CreateToken();
+    }
+}
